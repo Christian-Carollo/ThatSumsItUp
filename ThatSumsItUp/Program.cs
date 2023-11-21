7e5711a59869ccbc33f27209c9f6277fad6cc0b5
Administrator <Administrator@DESKTOP-95RB6IG>
2023-11-21 17:57:37 +0100

    Aggiungere i file di progetto.

---
+++ ThatSumsItUp/Program.cs
@@ -0,0 +1,4 @@
+﻿
+List<double> listaDouble = new() { 10.5, 1.9, 3.6, 9.3, 7.8, 5.5 };
+
+Console.WriteLine($"Risultato Numero Elmenti: {listaDouble.Count}\nRisultato Media: {listaDouble.Average()}\nRisultato Somma: {listaDouble.Sum()}");
